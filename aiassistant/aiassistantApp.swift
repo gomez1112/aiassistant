71ce79f83126fb8a60de38890a7b08ad55292460
gomez1112 <54489633+gomez1112@users.noreply.github.com>
2026-02-20 13:08:25 -0500

    Initial Commit

---
+++ aiassistant/aiassistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  aiassistantApp.swift
+//  aiassistant
+//
+//  Created by Gerard Gomez on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct aiassistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
